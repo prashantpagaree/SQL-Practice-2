811d6b8912876af6980cbceae4cb77b289ba0663
Prashant Sanjay Pagare <104510947+prashantpagaree@users.noreply.github.com>
2022-08-23 17:07:34 +0530

    Add files via upload

---
+++ Assignment-6.sql
@@ -0,0 +1,68 @@
+create database assign6;
+use assign6;
+select * from Students;
+
+--1a. Get first 3 characters in the student name
+
+select student_name, LEFT(Student_Name,3) as Student_New from Students;
+
+--1b. Convert all the student names into Upper and Lower case.Create both the columns in same table
+
+select *, upper(Student_Name)as Stu_Name_Uppercase,LOWER(Student_Name) as Stu_Name_Lowercase from Students;
+
+--1c. Reverse all the student names(writing the name from right to left)
+
+select REVERSE(Student_Name) Stu_Name_Reverse from Students;
+
+--1d. Add the section at the end of each student’s name along with ‘-’Example: Gabriela Benitez-A
+
+select CONCAT(student_name, '-',section) as Stu_Name_with_Section from students;
+
+--1e. Find the length of name of students
+
+select Student_Name,LEN(student_name) as length from Students;
+
+--1f. Find the Max & Min scores of each subject in the class
+
+select max(SQL_Marks)as Max_SQL_Marks ,min(SQL_Marks) as Min_SQL_Marks,max(Excel_Marks) as Max_Excel_Marks,
+min(Excel_Marks) as min_Excel_Marks,max(Python_Marks) as max_Python_Marks ,min(Python_Marks) as min_Python_Marks from Students;
+
+--1g. Find the Average score of each subject in the class
+
+select avg(SQL_Marks)as Avg_SQL_Marks,Avg(Excel_Marks) as Avg_Excel_Marks,
+Avg(Python_Marks) as Avg_Python_Marks from Students;
+
+--1h. Find the total no of students present in the class
+
+select count(ID) as Total_Students from Students;
+
+--1i. Find the day, month, year of joining of each student in the class separately.
+
+select Joining_Date, day(Joining_Date) as Day_Of_Joining_Date,month(Joining_Date) as Month_Of_Joining_Date,
+year(Joining_Date) as Year_Of_Joining_Date from Students;
+
+--1j. Find the number of days of coaching each student has taken.
+
+select Joining_Date, Leaving_Date, DATEDIFF(Day,Joining_Date,Leaving_Date) as Total_Coaching_Days from Students;
+
+--2a. How many students passed Excel
+
+select Count(Excel_Pass_Fail) as Excel_Passed_Students from Students where Excel_Pass_Fail = 'Pass';
+
+--2b. How many students failed Python
+
+select Count(Python_Pass_Fail) as Python_Failed_Students from Students where Python_Pass_Fail = 'Fail';
+
+--2c. No of students who passed Excel in each of the joining months
+
+Select month(Joining_Date) as Join_Month,count(Excel_Pass_Fail) as Total_Excel_Pass_Stud from Students where Excel_Pass_Fail='Pass' group by month(Joining_Date);
+
+--2d. No of students who failed SQL in each of the joining months
+
+Select month(Joining_Date) as Join_Month,count(SQL_Pass_Fail) as Total_SQL_Fail_Stud from Students where SQL_Pass_Fail='Fail' group by month(Joining_Date);
+
+--2e. Find the section wise no of students failed in Python
+
+select Section,count(Python_Pass_Fail) as Python_Fail_Stud from Students where Python_Pass_Fail ='Fail' group by section;
+
+--2f. Segregate the A section students month wiseselect Month(Joining_Date) As Joining_Month,count(ID) as Total_Stu  from Students where section = 'A' GROUP BY MONTH(Joining_Date);
---
+++ Assignment-6O.sql
@@ -0,0 +1,58 @@
+use assign6;
+select * from Students order by Joining_Date ;
+
+--2a. Students whose names start with A and who joined in march
+
+select Student_Name,month(Joining_Date) as Join_March from Students where Student_Name like 'A%'and Month(Joining_Date)=3; 
+
+--2b. Students having C in their name and at least 7 characters in their name.
+
+select Student_Name from Students where Student_Name like '%c%' and len(Student_Name)>=7;
+
+--2c. In which month the maximum number of students registered
+
+select top 1 month(Joining_Date)as join_month, count(id) as total_regi_stud  from Students GROUP BY month(Joining_Date) Order by count(id) desc;
+
+--d. Student with minimum duration(days, and months) in EXCEL,SQL and python.
+
+select top 1 Student_Name, datediff(day,Joining_Date,Leaving_Date) as Datediff from students order by datediff(day,Joining_Date,Leaving_Date);
+
+--e. Section wise average duration (days and months) of students in course.
+
+select section, avg(datediff(day,Joining_Date,Leaving_Date)) as Avg_Datediff from students group by Section  ;
+
+--f. Give a month wise summary of number of registrations
+
+select Month(Joining_Date) as Month ,count(ID) as no_of_regis from Students group by Month(Joining_Date);
+
+--g. How many students joined in the last 45 days. Compare this with that of the first 45 days.
+
+Select Count(ID) as Total_Reg_Stud from Students WHERE Joining_Date > (select dateadd(day,-45,max(Joining_Date)) from Students);
+
+Select Count(ID) as Total_Reg_Stud from Students WHERE Joining_Date < (select dateadd(day,45,min(Joining_Date)) from Students);
+
+--h. Get all records with an even date of registration.
+
+select *,day(Joining_Date) as Even_Day from Students where day(Joining_Date) like '%[02468]';
+
+--i. Get all records Starting from the first date with a difference in dates of 5.
+
+select min(joining_date) from students;
+select * from Students where Joining_Date between '2014-01-02' and '2014-01-07';
+
+--j. Get all records from the 10th day of every month. (apply 3 different methods to get answers in 3 different approaches )
+
+select * from Students where DAY(Joining_Date) like '_0';
+select * from Students where DATEPART(day,Joining_Date) like '_0';
+select * from Students where convert(varchar(25), Joining_Date ) like '%0';
+select * from Students where cast(Joining_Date as varchar(25) ) like '%0';
+
+--k. Convert the registration column format from this ‘YYYY-MM-DD’ to ‘YYYYMMDD’. (Example ‘2014-02-05’ becomes ‘20140205)
+select Joining_Date, REPLACE(CAST(Joining_Date as Varchar(30)),'-','') as New_Join_Date from Students;
+
+--l. WAQ to print SQL Marks column in marks/100 format.(Example 89 becomes 89/100)
+select SQL_Marks, CONCAT(SQL_Marks,'/','100') as SQL_Percentage from Students;
+
+--m. Print a code in a new column which follows: SQL-Excel-Python Marks (Example 70, 92, 44 becomes 70-92-44)
+
+select *, CONCAT_WS('-', SQL_Marks,Excel_Marks,Python_Marks) as New_Marks from Students;
