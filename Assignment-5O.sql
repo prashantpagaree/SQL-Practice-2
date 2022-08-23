fd930be7f55841139723c4d630432a2f039bb9f0
Prashant Sanjay Pagare <104510947+prashantpagaree@users.noreply.github.com>
2022-08-23 17:05:44 +0530

    Update Assignment-5O.sql

--- Assignment-5O.sql
+++ Assignment-5O.sql
@@ -1,4 +1,42 @@
 use assign5;
+
+--create table EmployeeInfo
+
+create table EmployeeInfo(
+EmpID Int,
+EmpFname char(10),
+EmpLname char(10),
+Department char(10),
+Project varchar(5),
+Address varchar(25),
+DOB DATE,
+Gender char(2));
+
+select * from EmployeeInfo;
+insert into EmployeeInfo
+values(1,'Sanjay', 'Mehra', 'HR', 'P1', 'Hyderabad(Hyd)','01/12/19','M'),
+(2,'Ananya', 'Mishra', 'Admin', 'P2', 'Delhi(DEL)','02/05/19','F'),
+(3,'Rohan', 'Diwan', 'Account', 'P3', 'Mumbai(BOM)','01/12/19','M'),
+(4,'Sonia', 'Kulkarni', 'HR', 'P1', 'Hyderabad(Hyd)','01/12/19','F'),
+(5,'Ankit', 'Kapoor', 'Admin', 'P2', 'Delhi(DEL)','01/12/19','M');
+
+--create table EmployeePosition
+
+create table EmployeePosition(
+EmpID Int,
+EmpPosition char(10),
+DateOfjoing date,
+salary int );
+
+select * from EmployeePosition;
+
+insert into EmployeePosition
+values(1,'Manager','01/05/2022',500000 ),
+(2,'Executive','02/05/2022',75000 ),
+(3,'Manager','01/05/2022',90000 ),
+(4,'Lead','02/05/2022',85000 ),
+(5,'Executive','01/05/2022',300000 );
+
 select * from employeeposition;
 select * from employeeinfo;
 
